5e0bbdc012fd9ffbe74e50816e44f7085360b3f4
Oghenevwaire Onobrudu <118831423+Yray-6@users.noreply.github.com>
2024-10-09 15:13:16 +0100

    Update Questions.tsx added shuffle

--- components/Questions.tsx
+++ components/Questions.tsx
@@ -3,6 +3,11 @@
 import React, { useState } from "react";
 import { questions } from "@/questions";
 
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
+
 export default function Questions() {
   const [selectedOption, setSelectedOption] = useState<{ [key: number]: string }>({});
   const [showAnswer, setShowAnswer] = useState<{ [key: number]: boolean }>({});
@@ -33,6 +38,10 @@ export default function Questions() {
     });
   };
 
+  const handleShuffle = (arr)=>{
+    shuffle(arr);
+  }
+
   const indexOfLastQuestion = currentPage * questionsPerPage;
   const indexOfFirstQuestion = indexOfLastQuestion - questionsPerPage;
   const currentQuestions = questions.slice(indexOfFirstQuestion, indexOfLastQuestion);
@@ -44,6 +53,7 @@ export default function Questions() {
         <h2 className="text-2xl lg:text-3xl font-semibold text-blue-900 mb-4">
           Welcome to the Practice Test
         </h2>
+        <button onClick=(()=>{handleShuffle(currentQuestions)})>Shuffle</button>
         <p className="text-base lg:text-lg text-gray-700 mb-2">
           This practice test will help you prepare for the CPG exam. Carefully read each question and choose the best answer.
         </p>
